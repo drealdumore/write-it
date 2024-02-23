21944b64066e17c091bc386364a26ad25c8f9b0e
drealdumore <sireroyce1@gmail.com>
2024-02-23 13:09:18 +0100

    analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoaderComponent } from './components/loader/loader.component';
 import { LoaderService } from './services/loader.service';
 import { CommonModule } from '@angular/common';
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoaderComponent, CommonModule],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    LoaderComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  private loaderService = inject(LoaderService);
+  constructor(private loaderService: LoaderService) {}
+  // private loaderService = inject(LoaderService);
 
   loaderSignal = this.loaderService.loaderSignal;
 }
